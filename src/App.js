a1bead73c5035acc10feaed547b70de47a49ef60
tpisfil <tpisfil13@gmail.com>
2022-03-10 16:31:45 -0500

    Hardcoded values, prepping for API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, {useState} from "react";
+import PokemonList from "./PokemonList";
 
 function App() {
+
+  const [pokemon, setPokemon] = useState(["bulba","squirt","char"]);
+
   return (
-    null
+    <PokemonList pokemon={pokemon}/>
+    //1. sending the current pokemon list down to PokemonList.js as a prop
   );
 }
 
---
+++ src/PokemonList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PokemonList = ({pokemon}) => {
+//1. destructuring pokemon from props.pokemon to use it here
+//if you didnt do this you would have to type props everytime 
+    return (
+        <div>
+            {pokemon.map( (poke,index) =>
+                <p key={index}>{poke}</p>
+            )}
+        </div>
+    );
+};
+
+export default PokemonList;
