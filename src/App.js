2d33f0c2d2c4753cf1ff465eea8fa110ea6ea301
tpisfil <tpisfil13@gmail.com>
2022-03-11 15:18:47 -0500

    Finished the API call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
 import axios from "axios";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   console.log("do we reach the top of the app component?");
   const [pokemon, setPokemon] = useState([]);
   // const [pokemon, setPokemon] = useState(["bulba","squirt","char"]);
-  const [currentPageURL, setCurrentPageURL] = useState("https://pokeapi.co/api/v2/pokemon/?limit=50");
+  const [currentPageURL, setCurrentPageURL] = useState("https://pokeapi.co/api/v2/pokemon/?limit=150");
   const [prevPageURL, setPrevPageURL] = useState();
   const [nextPageURL, setNextPageURL] = useState();
   const [loading, setLoading] = useState(true);
@@ -15,33 +16,47 @@ function App() {
   useEffect(() => {
     setLoading(true);
     console.log("loading is now true")
-    axios.get(currentPageURL).then(res => {
+    let cancel; 
+    axios.get(currentPageURL, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
       console.log("finally finished axios");
       // console.log(res.data);
       console.log("pokemon is now: " + pokemon);
       setLoading(false);
       console.log("im assuming we leave here to re render or naw??");
-      setPrevPageURL(res.data.previous);
+      setPrevPageURL(res.data.previous); 
       setNextPageURL(res.data.next);
       setPokemon(res.data.results);
       console.log("just set poke to res so now its: " + pokemon);
     })
-    // return () => {
-    //   cleanup
-    // }
+    return () => cancel();
+    //this return will run when useEffect is invoked again
   }, [currentPageURL]);
   //everytime the currentPageURL changes, this component will rerender 
 
+  function goToNextPage(){
+    setCurrentPageURL(nextPageURL)
+  }
+  function goToPrevPage(){
+    setCurrentPageURL(prevPageURL)
+  }
+
   if (loading) {
-    console.log("LOADING...");//return "loading..."
-    return "Loading"};
+    console.log("LOADING...");
+    return "Loading..."};
 
   console.log("poke during loading" + pokemon);
   return (
     console.log("we're in the APP return statement"),
     console.log("poke that gets sent to other component is: " + pokemon),
-    <PokemonList pokemon={pokemon}/>
-    //1. sending the current pokemon list down to PokemonList.js as a prop
+    <>
+      <PokemonList pokemon={pokemon}/>
+      <Pagination
+        goToNextPage ={nextPageURL ? goToNextPage : null}
+        goToPrevPage ={prevPageURL ? goToPrevPage : null}
+      />
+    </>
   );
 }
 
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({goToNextPage, goToPrevPage}) => {
+
+
+
     return (
         <div>
-            
+            {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
         </div>
     );
 };
